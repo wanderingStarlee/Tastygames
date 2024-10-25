27b4cb0d54dfc6d11b8dee2ca39771cc3409a805
Гончар Анатолий <bloodsaiferpro@gmail.com>
2024-10-25 22:38:59 +0500

    Add files via upload

---
+++ MiniBulls/src/test/java/com/example/MiniBulls/MiniBullsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MiniBulls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBullsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
